0cb14ac03e8a0a50942ccbc462456f1e73fe27f3
Kranti Kumari <krantisingh169@gmail.com>
2023-12-12 16:54:07 +0530

    Update get_current_utc.c

--- time/get_current_utc.c
+++ time/get_current_utc.c
@@ -1,10 +1,22 @@
 // C program to get current UTC time
+/* 
+struct tm {
+    int tm_sec; // seconds (from 0 to 59)
+    int tm_min; // minutes (from 0 to 59)
+    int tm_hour; // hours (from 0 to 23)
+    int tm_mday; // day of the month
+    int tm_mon; // month (from 0 to 11)
+    int tm_year; // year (number of years since 1990)
+    int tm_wday; // day of the week (from 0 to 6)
+    int tm_yday; // day in the year (from 0 to 365)
+    int tm_isdst; // daylight saving time
+};
+*/
 
-#include <stdio.h>
-#include <time.h>
+#include<stdio.h>
+#include<time.h>
 
-int main()
-{
+int main() {
     time_t tmi;
     struct tm* utcTime;
 
