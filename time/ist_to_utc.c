c0232865b3245bed82d8de029339d211a6dd2228
Kranti Kumari <krantisingh169@gmail.com>
2023-12-12 16:48:29 +0530

    Update ist_to_utc.c

--- time/ist_to_utc.c
+++ time/ist_to_utc.c
@@ -1,27 +1,25 @@
 /* Convert IST time to UTC time */
-#include <stdio.h>
-#include <time.h>
+#include<stdio.h>
+#include<time.h>
 
-int main ()
-{
+int main() {
   time_t rawtime;
   
   /* 1st approach
   struct tm * timeinfo, *utctime;
 
-  time ( &rawtime );
-  timeinfo = localtime ( &rawtime );
-  printf ( "Current local time and date: %s", asctime (timeinfo) );
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  printf("Current local time and date: %s", asctime(timeinfo));
 
   utctime = gmtime (&rawtime);
   printf("UTC time and date: %s", asctime(utctime));
   */
 
-  /* 2nd approach
+  /* 2nd approach */
   time(&rawtime);
   printf("Current IST time and date: %s", asctime(localtime(&rawtime)));
   printf("Current UTC time and date: %s", asctime(gmtime(&rawtime)));
-  */
   
   return 0;
 }
